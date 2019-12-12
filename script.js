8d1113144da2a4173c3dcfa4772d125a42867a95
Andrei <andrewten22@yandex.ru>
2019-12-12 15:29:14 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+'use strict';
+
+// var leftBorderWidth = 1;
+
+// let something;
+// console.log(leftBorderWidth);
+
+// let person = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// }
+
+// console.log(person.name);
+// console.log(person["age"]);
+
+// let arr = ['1.png','2.png','3.png'];
+
+// // console.log(arr[0]);
+
+// // alert("hello world");
+
+// // let text = confirm("are you alex?");
+
+// // console.log(text);
+
+// let text = +prompt("ТЫ кто по жизни?", "бродяга");
+
+// console.log(text);
+// console.log(typeof(text));
+
+// console.log(4 + "- object");
+
+let incr = 10,
+    decr = 10;
+
+
+console.log(incr++,decr--);
+
+console.log(7%2);
+
+let isChecked = true;
+let isClose = false;
+
+console.log(!isChecked || isClose);
